5f500d5822a8529124a144f8c0ba30706c067e36
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-05-05 20:12:11 +0300

    AndParameterizedTest

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -1,28 +1,31 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestServiceTest {
-    @Test
-    void shouldCalculateRestMonthOne() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000,3000,20000,3",
+//            "100000,60000,150000,2"
+//    })
+    @CsvFileSource(files = "src/test/resources/restmonth.csv")
+    public void shouldCalculateRestMonthOne(int income, int expenses, int threshold, int expected) {
         RestService service = new RestService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+//
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-
-
- @Test
- void shouldCalculateRestMonthsTwo() {
-    RestService service = new RestService();
-    int income = 100_000;
-    int expenses = 60_000;
-    int threshold = 150_000;
-    int expected = 2;
-    int actual = service.calculate(income, expenses, threshold);
-    Assertions.assertEquals(expected, actual);
 }
 
-}
+//  @Test
+//  void shouldCalculateRestMonthsTwo() {
+//   RestService service = new RestService();
+//   int income = 100_000;
+//   int expenses = 60_000;
+//    int threshold = 150_000;
+//    int expected = 2;
+//    int actual = service.calculate(income, expenses, threshold);
+//    Assertions.assertEquals(expected, actual);}
+
