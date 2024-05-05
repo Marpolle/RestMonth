9ed680bde5bb561b740403b01e226b0704285431
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-05-05 19:24:37 +0300

    Gitignore added

---
+++ src/main/java/RestService.java
@@ -0,0 +1,16 @@
+public class RestService {
+    public int calculate(int income, int expenses, int threshold) {
+
+        int count = 0;
+        int money = 0;
+        for (int month = 0; month < 12; month++) {
+            if (money < threshold) {
+                money = money + income - expenses;
+            } else {
+                money = (money - expenses) / 3;
+                count++;
+            }
+        }
+        return count;
+    }
+}
